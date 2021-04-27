b5af277f49747990498e312e6f55da22a689fd51
Harrison Waltersm <hpw5@hotmail.com>
2021-04-28 09:45:34 +1000

    Create tkinter root

---
+++ PPlaylist.py
@@ -0,0 +1,12 @@
+import sqlite3
+import os
+import csv
+import tkinter as tk
+
+# initialise tkinter
+root = tk.Tk()
+root.title("Database")
+root.geometry("400x400")
+
+# Loop main window
+root.mainloop()
