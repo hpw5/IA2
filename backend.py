31409785f01cf2cd71911078481060bb2e5c6140
Harrison Waltersm <hpw5@hotmail.com>
2021-05-14 11:40:41 +1000

    add energy to song table

--- backend.py
+++ backend.py
@@ -121,8 +121,10 @@ def import_csv(file_name):
                 acousticness_values = row[14]
                 # import danceability values
                 danceability_values = row[8]
-                song_list.append((id_values,name_values,acousticness_values,danceability_values))
-        sqlmanycommand("INSERT OR IGNORE INTO Songs (id, name, acousticness, danceability) VALUES (?,?,?,?)",song_list)
+                # import energy values
+                energy_values = row[9]
+                song_list.append((id_values,name_values,acousticness_values,danceability_values,energy_values))
+        sqlmanycommand("INSERT OR IGNORE INTO Songs (id, name, acousticness, danceability, energy) VALUES (?,?,?,?,?)",song_list)
 
 ## MAIN PROGRAM ##
 # Create tables if no exist
