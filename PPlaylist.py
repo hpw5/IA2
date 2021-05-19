bab981c0ded09e6187f30adf25b17e7530e625d5
Harrison Walters <hpw5@hotmail.com>
2021-05-20 09:33:13 +1000

    Add login section

--- PPlaylist.py
+++ PPlaylist.py
@@ -12,11 +12,23 @@ root.geometry("700x900")
 sign_in_frame = tk.Frame(master=root, bg="black", height=120)
 sign_in_frame.pack(side=tk.TOP, fill=tk.X)
 
+## Create login section
+username_text = tk.Label(master=sign_in_frame, text="Username: ")
+username_text.grid(column=0, row=0)
+password_text = tk.Label(master=sign_in_frame, text="Password: ")
+password_text.grid(column=0, row=1)
+username_field = tk.Entry(master=sign_in_frame, width=20)
+username_field.grid(column=1, row=0, padx=5)
+password_field = tk.Entry(master=sign_in_frame, width=20)
+password_field.grid(column=1, row=1)
+submit_button = tk.Button(master=sign_in_frame, text="Submit", width=10)
+submit_button.grid(column=0, row=2)
+register_button = tk.Button(master=sign_in_frame, text="Register", width=10)
+register_button.grid(column=0, row=3)
+
 # Create bottom frame
 welcome_frame = tk.Frame(master=root)
 welcome_frame.pack(side=tk.BOTTOM)
 
-=======
->>>>>>> parent of bd57b48 (Create top frame)
 # Loop main window
 root.mainloop()
