07d02a4a8e04a8595b8d2c57a47f7c26026e0a90
Harrison Walters <hpw5@hotmail.com>
2021-05-19 16:01:24 +1000

    (minor) add master=

--- PPlaylist.py
+++ PPlaylist.py
@@ -9,12 +9,14 @@ root.title("PPlaylist")
 root.geometry("700x900")
 
 # Create top frame
-sign_in_frame = tk.Frame(root, bg="black", height=120)
+sign_in_frame = tk.Frame(master=root, bg="black", height=120)
 sign_in_frame.pack(side=tk.TOP, fill=tk.X)
 
 # Create bottom frame
-welcome_frame = tk.Frame(root)
+welcome_frame = tk.Frame(master=root)
 welcome_frame.pack(side=tk.BOTTOM)
 
+=======
+>>>>>>> parent of bd57b48 (Create top frame)
 # Loop main window
 root.mainloop()
