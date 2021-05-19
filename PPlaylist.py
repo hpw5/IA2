b06a90553f1da7d5870faae651eecf4649413c71
Harrison Walters <hpw5@hotmail.com>
2021-05-20 09:44:03 +1000

    Change frame names

--- PPlaylist.py
+++ PPlaylist.py
@@ -9,26 +9,26 @@ root.title("PPlaylist")
 root.geometry("700x900")
 
 # Create top frame
-sign_in_frame = tk.Frame(master=root, bg="black", height=120)
-sign_in_frame.pack(side=tk.TOP, fill=tk.X)
+top_frame = tk.Frame(master=root, bg="black", height=120)
+top_frame.pack(side=tk.TOP, fill=tk.X)
 
 ## Create login section
-username_text = tk.Label(master=sign_in_frame, text="Username: ")
+username_text = tk.Label(master=top_frame, text="Username: ")
 username_text.grid(column=0, row=0)
-password_text = tk.Label(master=sign_in_frame, text="Password: ")
+password_text = tk.Label(master=top_frame, text="Password: ")
 password_text.grid(column=0, row=1)
-username_field = tk.Entry(master=sign_in_frame, width=20)
+username_field = tk.Entry(master=top_frame, width=20)
 username_field.grid(column=1, row=0, padx=5)
-password_field = tk.Entry(master=sign_in_frame, width=20)
+password_field = tk.Entry(master=top_frame, width=20)
 password_field.grid(column=1, row=1)
-submit_button = tk.Button(master=sign_in_frame, text="Submit", width=10)
+submit_button = tk.Button(master=top_frame, text="Submit", width=10)
 submit_button.grid(column=0, row=2)
-register_button = tk.Button(master=sign_in_frame, text="Register", width=10)
+register_button = tk.Button(master=top_frame, text="Register", width=10)
 register_button.grid(column=0, row=3)
 
 # Create bottom frame
-welcome_frame = tk.Frame(master=root)
-welcome_frame.pack(side=tk.BOTTOM)
+bottom_frame = tk.Frame(master=root)
+bottom_frame.pack(side=tk.BOTTOM)
 
 # Loop main window
 root.mainloop()
