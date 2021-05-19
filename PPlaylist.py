bd57b4844bb641b852510abbd7327f5bec75da93
Harrison Walters <hpw5@hotmail.com>
2021-05-19 15:44:14 +1000

    Create top frame

--- PPlaylist.py
+++ PPlaylist.py
@@ -12,5 +12,9 @@ root.geometry("700x900")
 sign_in_frame = tk.Frame(root, bg="black", height=120)
 sign_in_frame.pack(side=tk.TOP, fill=tk.X)
 
+# Create bottom frame
+welcome_frame = tk.Frame(root)
+welcome_frame.pack(side=tk.BOTTOM)
+
 # Loop main window
 root.mainloop()
