2f7849ae006e5c5afaa6fdb5b6fb80cbbbe29488
Harrison Walters <hpw5@hotmail.com>
2021-05-20 11:57:35 +1000

    Add register section

--- PPlaylist.py
+++ PPlaylist.py
@@ -7,7 +7,7 @@ from tkinter import ttk
 # initialise tkinter
 root = tk.Tk()
 root.title("PPlaylist")
-root.geometry("700x900")
+root.geometry("1300x900")
 
 # Create top frame
 top_frame = tk.Frame(master=root, bg="black", height=120)
@@ -20,17 +20,38 @@ register_tab = ttk.Frame(tab_control_top)
 tab_control_top.add(login_tab, text='Login')
 tab_control_top.add(register_tab, text='Register')
 tab_control_top.pack(side=tk.LEFT)
-## Create login section
+
+### Create login section
 username_text = tk.Label(master=login_tab, text="Username: ")
 username_text.grid(column=0, row=0)
 password_text = tk.Label(master=login_tab, text="Password: ")
 password_text.grid(column=0, row=1)
 username_field = tk.Entry(master=login_tab, width=20)
 username_field.grid(column=1, row=0, padx=5)
-password_field = tk.Entry(master=login_tab, width=20)
+password_field = tk.Entry(master=login_tab, width=20, show="*")
 password_field.grid(column=1, row=1)
 submit_button = tk.Button(master=login_tab, text="Submit", width=10)
 submit_button.grid(column=0, row=2)
+
+### Create register section
+name_text = tk.Label(master=register_tab, width=20, text="Name: ")
+name_text.grid(column=0, row=0)
+username_text = tk.Label(master=register_tab, text="Username: ")
+username_text.grid(column=0, row=1)
+password_text = tk.Label(master=register_tab, text="Password: ")
+password_text.grid(column=0, row=2)
+confirm_password_text = tk.Label(master=register_tab, text="Confirm password: ")
+confirm_password_text.grid(column=0, row=3)
+register_button = tk.Button(master=register_tab, text="Register", width=10)
+register_button.grid(column=0, row=4)
+name_field = tk.Entry(master=register_tab)
+name_field.grid(column=1, row=0)
+username_field = tk.Entry(master=register_tab)
+username_field.grid(column=1, row=1)
+password_field = tk.Entry(master=register_tab, show="*")
+password_field.grid(column=1, row=2)
+confirm_password_field = tk.Entry(master=register_tab,show="*")
+confirm_password_field.grid(column=1, row=3)
 
 # Create bottom frame
 bottom_frame = tk.Frame(master=root)
