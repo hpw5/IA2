b324dc632736f00c3918bb99933056655756c76f
Harrison Walters <hpw5@hotmail.com>
2021-05-23 21:04:11 +1000

    Add prefrences table

--- PPlaylist.py
+++ PPlaylist.py
@@ -200,12 +200,15 @@ root.geometry("1300x900")
 
 # Create tkinter variables
 songs_status = tk.StringVar(value="Status: Not loaded!")
+mode_selection = tk.StringVar(value="-")
+key_selection = tk.StringVar(value="-")
+genre_selection = tk.StringVar(value="-")
 
 # Create top frame
 top_frame = tk.Frame(master=root, bg="black", height=120)
 top_frame.pack(side=tk.TOP, fill=tk.X)
 
-## Create tabs for top frame
+# Create tabs for top frame
 tab_control_top = ttk.Notebook(top_frame)
 login_tab = ttk.Frame(tab_control_top)
 register_tab = ttk.Frame(tab_control_top)
@@ -213,7 +216,7 @@ tab_control_top.add(login_tab, text='Login')
 tab_control_top.add(register_tab, text='Register')
 tab_control_top.pack(side=tk.LEFT)
 
-### Create login section
+# Create login section
 username_text = tk.Label(master=login_tab, text="Username: ")
 username_text.grid(column=0, row=0)
 password_text = tk.Label(master=login_tab, text="Password: ")
@@ -225,7 +228,7 @@ password_field.grid(column=1, row=1)
 submit_button = tk.Button(master=login_tab, text="Submit", width=10)
 submit_button.grid(column=0, row=2)
 
-### Create register section
+# Create register section
 name_text = tk.Label(master=register_tab, width=20, text="Name: ")
 name_text.grid(column=0, row=0)
 username_text = tk.Label(master=register_tab, text="Username: ")
@@ -249,7 +252,7 @@ confirm_password_field.grid(column=1, row=3)
 bottom_frame = tk.Frame(master=root)
 bottom_frame.pack(side=tk.TOP, anchor=tk.NW)
 
-## Create tabs for bottom frame
+# Create tabs for bottom frame
 tab_control_bottom = ttk.Notebook(bottom_frame)
 import_tab = ttk.Frame(tab_control_bottom)
 create_playlist_tab = ttk.Frame(tab_control_bottom)
@@ -257,13 +260,13 @@ tab_control_bottom.add(import_tab, text='Start')
 tab_control_bottom.add(create_playlist_tab, text="Create playlist")
 tab_control_bottom.pack()
 
-### Create welcome text
+# Create welcome text
 welcome_text = tk.Label(master=import_tab, text="Welcome. ", font="Helvetica, 35")
 welcome_text.pack()
 info_text = tk.Label(master=import_tab, text= "Import the spotify catalogues to get started.", font="Helvetica, 20")
 info_text.pack()
 
-### Create import buttons
+# Create import buttons
 import_songs_frame = tk.Frame(master=import_tab)
 import_songs_frame.pack(anchor=tk.W)
 blank_1 = tk.Label(master=import_songs_frame)
@@ -290,7 +293,7 @@ import_artists_button.pack(anchor=tk.W)
 import_artists_status = tk.Label(master=import_artists_frame, textvariable=songs_status, fg="red")
 import_artists_status.pack(anchor=tk.W)
 
-## Create prefrences side
+# Create prefrences side
 prefrences_frame = tk.Frame(master=create_playlist_tab)
 prefrences_frame.pack(side=tk.LEFT,anchor=tk.NW)
 create_prefrences_label = tk.Label(master=prefrences_frame, text="Create playlist", font="Helvetica, 35")
@@ -298,5 +301,131 @@ create_prefrences_label.pack()
 prefrences_guide_label = tk.Label(master=prefrences_frame, text="Fill out the fields with what audio features you want your music to have.", font="Helvetica, 15")
 prefrences_guide_label.pack()
 
+# Create prefrences table
+prefrences_table_frame = tk.Frame(master=prefrences_frame)
+prefrences_table_frame.pack(anchor=tk.W)
+audio_feature_label = tk.Label(master=prefrences_table_frame, text="Audio Feature")
+audio_feature_label.grid(row=0, column=1)
+audio_feature_value_label = tk.Label(master=prefrences_table_frame, text="Value")
+audio_feature_value_label.grid(row=0, column=2)
+# Acousticness
+acousticness_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+acousticness_checkbox.grid(row=1, column=0)
+acousticness_label = tk.Label(master=prefrences_table_frame, text="Acousticness")
+acousticness_label.grid(row=1, column=1)
+acousticness_entry = tk.Entry(master=prefrences_table_frame)
+acousticness_entry.grid(row=1, column=2)
+acousticness_example = tk.Label(master=prefrences_table_frame, text="0-100")
+acousticness_example.grid(row=1, column=3)
+# Danceability
+danceability_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+danceability_checkbox.grid(row=2, column=0)
+danceability_label = tk.Label(master=prefrences_table_frame, text="Danceability")
+danceability_label.grid(row=2, column=1)
+danceability_entry = tk.Entry(master=prefrences_table_frame)
+danceability_entry.grid(row=2, column=2)
+danceability_example = tk.Label(master=prefrences_table_frame, text="0-100")
+danceability_example.grid(row=2, column=3)
+# Energy
+energy_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+energy_checkbox.grid(row=3, column=0)
+energy_label = tk.Label(master=prefrences_table_frame, text="Energy")
+energy_label.grid(row=3, column=1)
+energy_entry = tk.Entry(master=prefrences_table_frame)
+energy_entry.grid(row=3, column=2)
+energy_example = tk.Label(master=prefrences_table_frame, text="0-100")
+energy_example.grid(row=3, column=3)
+# Duration_ms
+duration_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+duration_checkbox.grid(row=4, column=0)
+duration_label = tk.Label(master=prefrences_table_frame, text="Duration in seconds")
+duration_label.grid(row=4, column=1)
+duration_entry = tk.Entry(master=prefrences_table_frame)
+duration_entry.grid(row=4, column=2)
+duration_example = tk.Label(master=prefrences_table_frame, text="")
+# Instrumentalness
+intrumentalness_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+intrumentalness_checkbox.grid(row=5, column=0)
+intrumentalness_label = tk.Label(master=prefrences_table_frame, text="Instrumentalness")
+intrumentalness_label.grid(row=5, column=1)
+intrumentalness_entry = tk.Entry(master=prefrences_table_frame)
+intrumentalness_entry.grid(row=5, column=2)
+intrumentalness_example = tk.Label(master=prefrences_table_frame, text="0-100")
+intrumentalness_example.grid(row=5, column=3)
+# Valance
+valance_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+valance_checkbox.grid(row=6, column=0)
+valance_label = tk.Label(master=prefrences_table_frame, text="Valance (Happiness)")
+valance_label.grid(row=6, column=1)
+valance_entry = tk.Entry(master=prefrences_table_frame)
+valance_entry.grid(row=6, column=2)
+valance_example = tk.Label(master=prefrences_table_frame, text="0-100")
+valance_example.grid(row=6, column=3)
+# Popularity
+popularity_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+popularity_checkbox.grid(row=7, column=0)
+popularity_label = tk.Label(master=prefrences_table_frame, text="Popularity")
+popularity_label.grid(row=7, column=1)
+popularity_entry = tk.Entry(master=prefrences_table_frame)
+popularity_entry.grid(row=7, column=2)
+popularity_example = tk.Label(master=prefrences_table_frame, text="0-100")
+popularity_example.grid(row=7, column=3)
+# Tempo
+tempo_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+tempo_checkbox.grid(row=8, column=0)
+tempo_label = tk.Label(master=prefrences_table_frame, text="Speed (tempo)")
+tempo_label.grid(row=8, column=1)
+tempo_entry = tk.Entry(master=prefrences_table_frame)
+tempo_entry.grid(row=8, column=2)
+# Loudness
+loudness_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+loudness_checkbox.grid(row=9, column=0)
+loudness_label = tk.Label(master=prefrences_table_frame, text="Loudness")
+loudness_label.grid(row=9, column=1)
+loudness_entry = tk.Entry(master=prefrences_table_frame)
+loudness_entry.grid(row=9, column=2)
+loudness_example = tk.Label(master=prefrences_table_frame, text="0-100")
+loudness_example.grid(row=9, column=3)
+# Speechiness
+speechiness_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+speechiness_checkbox.grid(row=10, column=0)
+speechiness_label = tk.Label(master=prefrences_table_frame, text="Speechiness")
+speechiness_label.grid(row=10, column=1)
+speechiness_entry = tk.Entry(master=prefrences_table_frame)
+speechiness_entry.grid(row=10, column=2)
+speechiness_example = tk.Label(master=prefrences_table_frame, text="0-100")
+speechiness_example.grid(row=10, column=3)
+# Mode
+mode_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+mode_checkbox.grid(row=11, column=0)
+mode_label = tk.Label(master=prefrences_table_frame, text="Mode")
+mode_label.grid(row=11, column=1)
+mode_options = ["Major", "Minor"]
+mode_dropdown = tk.OptionMenu(prefrences_table_frame, mode_selection, *mode_options)
+mode_dropdown.grid(row=11, column=2)
+# Key
+key_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+key_checkbox.grid(row=12, column=0)
+key_label = tk.Label(master=prefrences_table_frame, text="Key")
+key_label.grid(row=12, column=1)
+key_options = ["C", "C#", "D", "D#", "E", "E#", "F", "F#", "G", "G#", "A", "A#", "B"]
+key_dropdown = tk.OptionMenu(prefrences_table_frame, key_selection, *key_options)
+key_dropdown.grid(row=12, column=2)
+# Genre
+#TODO Put genres into dropdown
+genre_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+genre_checkbox.grid(row=13, column=0)
+genre_label = tk.Label(master=prefrences_table_frame, text="Genre")
+genre_label.grid(row=13, column=1)
+genre_options = ["Placeholder"]
+genre_dropdown = tk.OptionMenu(prefrences_table_frame, genre_selection, *genre_options)
+genre_dropdown.grid(row=13, column=2)
+# Number of songs
+num_of_songs_checkbox = tk.Checkbutton(master=prefrences_table_frame)
+num_of_songs_checkbox.grid(row=14, column=0)
+num_of_songs_label = tk.Label(master=prefrences_table_frame, text="Number of songs")
+num_of_songs_label.grid(row=14, column=1)
+num_of_songs_entry = tk.Entry(master=prefrences_table_frame)
+num_of_songs_entry.grid(row=14, column=2)
 # Loop main window
 root.mainloop()
